282675d562224285143c9fa9e4a0bfa25031cd91
Alejandro <106261416+alxxch@users.noreply.github.com>
2025-04-25 18:32:13 +0200

    SP1_25042025 - Admin requisitos mínimos y reorganización

--- ComandEat/android/app/src/main/kotlin/com/comandeat/comandeat/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.comandeat.comandeat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- ComandEat/lib/core/services/providers.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'menu_repository.dart';
-import 'pedido_repository.dart';
-
-final pedidoRepoProvider = Provider<PedidoRepository>((_) => PedidoRepository());
-
-final menuRepoProvider = Provider<MenuRepository>((_) => MenuRepository());
